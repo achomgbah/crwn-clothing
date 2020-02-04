ad501683fe10132ca9d018fab19eb92477cae4ca
robinson <mgbahacho@aol.com>
2020-02-04 10:31:23 +0100

    Implemented cart utility

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,27 @@
 import {CartTypes} from "./cart.types";
+import {addItemToCart} from "./cart.utils";
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    if (action.type === CartTypes.TOGGLE_CART_HIDDEN) {
-        return {
-            ...state,
-            hidden: !state.hidden
-        };
-    } else {
-        return state;
+    switch (action.type) {
+        case CartTypes.TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            };
+        case CartTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+
+        default:
+            return state;
+
     }
 };
 export default cartReducer;
